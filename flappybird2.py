e5ef925c61522999dfcb4f08f330b09541130519
Shivam Kafle <shivamkafle02@gmail.com>
2025-01-05 13:41:34 +0000

    Update flappybird2.py

--- flappybird2.py
+++ flappybird2.py
@@ -1,16 +1,3 @@
-# http://christianthompson.com
-# Twitter: @tokyoedtech
-
-# Welcome to my live coding / Q&A Session for October 12, 2019.
-# If you have any Python questions - ask in chat
-# Please provide any code via pastebin.com link
-
-# OS: Ubuntu Linux 19.04
-# Programming Editor: Geany
-
-# Live coding October 12, 2019 (Typhoon Hagibis Edition)
-# Flappy Bird Using Python 3 and the Turtle Module
-
 import turtle
 import time
 
